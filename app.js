5f4f06b2d847761b46bde2a0aea24e73be037d32
Hathaway-Shan <hathaway.shan@gmail.com>
2022-06-10 18:48:55 -0700

    changed p text output to a ul that botton could push to tried tto add a rest function for page elements and function to stop text input pushing blanks to the array failed at both calling it a night

--- app.js
+++ app.js
@@ -4,46 +4,44 @@
 function getDefaultCity() {
     let city = {
         skyline: 'city-sun',
-        background: 'neutral-back',
+        background: 'sunny-back',
         name: 'New City',
-        slogan: '',
+        slogan: [],
+        theme: 'neutral'
     };
     return city;
 }
-
+//set a new variable city equal to getDefaultCity function
 let city = getDefaultCity();
-
-
-
+//Empty array for the share button to push to
+let cities = [];
 
 //city inputs section root element
 const inputSection = document.getElementById('input-selection');
 //inputSection children can be querySelected from root element 
 const nameInput = inputSection.querySelector('input');
 const sloganInput = inputSection.querySelector('textarea');
-const backgroundDisplay = document.getElementById('image-background-display')
-// const shareButton = inputSection.querySelector('button');
+const shareButton = inputSection.querySelector('button');
 
-//children that share a class can be entered into an array
+//children that share a class can be entered into an array to call them in order from left to right
+const backgroundDisplay = document.getElementById('image-background-display');
 const [citySelect, backgroundSelect] = inputSection.querySelectorAll('select');
-//add event listeners to define undefined constants
 const [cityImage, backgroundImage] = backgroundDisplay.querySelectorAll('img');
 //displaySection children
 let cityName = backgroundDisplay.querySelector('h2');
-// let cityImage = backgroundDisplay.querySelector('img');
-let citySlogan = backgroundDisplay.querySelector('p');
-// let backgroundImage = document.getElementsByClassName('back-image');
+// let citySlogan = backgroundDisplay.querySelector('ul');
 
+//eventListener section
 nameInput.addEventListener('input', () => {
     city.name = nameInput.value;
     newCity();
 });
 sloganInput.addEventListener('input', () => {
     city.slogan = sloganInput.value;
-    newCity();
+    
+    newCity();  
 });
 citySelect.addEventListener('change', () => {
-    console.log(citySelect.value);
     city.skyline = citySelect.value;
     newCity();
     
@@ -52,37 +50,69 @@ backgroundSelect.addEventListener('change', () => {
     city.background = backgroundSelect.value;
     newCity();
 });
-// push function for button under construction please be patient
-// shareButton.addEventListener('click', () => {
 
-// });
+shareButton.addEventListener('click', () => {
+    sloganSum();
+//add created object to array
+    cities.push(city);
+//display new cities list
+    displayCities();
+//reset the original city display
+    city = newCity();
+//reset display functions 
+    newCity();
+});
+
+function sloganSum() {
+    const slogan = sloganInput.value;
+    if (!slogan) {
+        return;
+    }
+} 
+
 
 function newCity() {
-    //clears previous state
-    console.log(city.skyline);
-    console.log(city.background);
-    //city.skyline holds initial value from object
+//clears previous state
     backgroundDisplay.classList.value = '';
-    //displays new selections and inputs
+//displays new selections and inputs
     cityName.textContent = city.name;
-    citySlogan.textContent = city.slogan;
-    //use backticks for images
+    // citySlogan.textContent = city.slogan;
+//use backticks for images
     backgroundImage.src = `./assets/${city.background}.png`;
     cityImage.src = `./assets/${city.skyline}.png`;
-    // backgroundDisplay.classList.add(city.background);
+    backgroundDisplay.classList.add(city.theme);
     
 }
 
-function displayCity() {
-    city.name = cityName.value;
-    city.skyline = cityImage.value;
-    city.slogan = citySlogan.value;
+function resetCity() {
+    cityName.value = city.name;
+    cityImage.value = city.skyline;
+    city.Slogan = city.slogan;
 }
+
+const displaySection = document.getElementById('slogan-text-area');
+const list = displaySection.querySelector('ul');
+
+function displayCities() {
 //
-// nameInput.value = city.name;
-// sloganInput.value = city.slogan;
-// themeSelect.value = city.theme;
-// citySelect.value = city.skyline;
+    list.innerHTML = '';
+//iterate through the state of the object
+    for (const city of cities) {
+//creates a new list 
+        const li = document.createElement('li');
+//create new spans that read the properties of a new city
+        const spacer = ': ';
+        const cityName = document.createElement('span');
+        cityName.textContent = city.name;
+        const citySlogan = document.createElement('span');
+        citySlogan.textContent = city.slogan;
+//add new spans to new li element
+        li.append(cityName, spacer, citySlogan);
+//add new item to list li
+        list.append(li);
+    } 
+}
+
     
 
 // }
